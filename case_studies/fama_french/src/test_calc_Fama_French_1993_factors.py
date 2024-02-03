6b949442580685b95f5406ac8b0a4e8744860807
Jeremy Bejarano <jmbejara@gmail.com>
2024-02-03 16:53:39 -0500

    Remove index from unit tests

--- case_studies/fama_french/src/test_calc_Fama_French_1993_factors.py
+++ case_studies/fama_french/src/test_calc_Fama_French_1993_factors.py
@@ -55,29 +55,30 @@ def test_calculate_market_equity():
     crsp = load_CRSP_Compustat.load_CRSP_stock_ciz(data_dir=DATA_DIR)
     crsp = subset_CRSP_to_common_stock_and_exchanges(crsp)
     crsp2 = calculate_market_equity(crsp)
-    output = crsp2[["permco", "mthcaldt", "me"]].head().to_string()
+    output = crsp2[["permco", "mthcaldt", "me"]].head().to_string(index=False)
     # print(output)
     expected = """
-         permco   mthcaldt            me
-        1109388    7952 1986-01-31  16100.000000
-        1115063    7952 1986-02-28  11960.000000
-        1120747    7952 1986-03-31  16330.000000
-        1126446    7952 1986-04-30  15172.000000
-        1132153    7952 1986-05-30  11793.859375
+    permco   mthcaldt           me
+    7952 1986-01-31 16100.000000
+    7952 1986-02-28 11960.000000
+    7952 1986-03-31 16330.000000
+    7952 1986-04-30 15172.000000
+    7952 1986-05-30 11793.859375
     """
     assert output.replace(" ", "").replace("\n", "") == expected.replace(" ", "").replace("\n", "")
 
 def test_factors():
     vwret, vwret_n, ff_factors, ff_nfirms = create_Fama_French_factors(data_dir=DATA_DIR)
     expected = """
-        sbport       date        BH        BL        BM
-        733    2022-08-31 -0.014872 -0.046368 -0.018841
-        734    2022-09-30 -0.087952 -0.088352 -0.094143
-        735    2022-10-31  0.147654  0.059447  0.110792
-        736    2022-11-30  0.049567  0.048842  0.055844
-        737    2022-12-31 -0.049520 -0.067278 -0.043296
+        date        BH        BL        BM
+    2022-08-31 -0.014872 -0.046368 -0.018841
+    2022-09-30 -0.087952 -0.088352 -0.094143
+    2022-10-31  0.147654  0.059447  0.110792
+    2022-11-30  0.049567  0.048842  0.055844
+    2022-12-31 -0.049520 -0.067278 -0.043296
     """.replace(" ", "").replace("\n", "")
-    output = ff_factors[['date', 'BH', 'BL', 'BM']].tail().to_string().replace(" ", "").replace("\n", "")
+    output = ff_factors[['date', 'BH', 'BL', 'BM']].tail().to_string(index=False).replace(" ", "").replace("\n", "")
+    # print(output)
     assert expected == output
 
     
